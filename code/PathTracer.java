89412b824cd0f0e2d10399a19b8896178c489147
ConstArt <114258174+3ConstArt3@users.noreply.github.com>
2024-05-15 13:28:56 +0300

    Create PathTracer.java

---
+++ code/PathTracer.java
@@ -0,0 +1,54 @@
+class PathTracer
+{
+
+  private PVector position;
+
+  private ArrayList<PVector> path;
+  private PGraphics pathTracer;
+
+  private ChaoticPendulum system;
+
+  /* Constructor definition */
+  public PathTracer(PVector position, ChaoticPendulum system)
+  {
+    this.position = position;
+    this.path = new ArrayList<PVector>();
+    this.pathTracer = createGraphics(width, height);
+    
+    this.system = system;
+  }
+
+  /* Function definition */
+  public PGraphics getTracer()
+  {
+    return this.pathTracer;
+  }
+
+  public void update()
+  {
+    var pendulum = this.system.getBottomPendulum();
+    var ball = pendulum.getBall();
+    this.path.add(ball.getPosition());
+  }
+
+  public void render()
+  {
+    this.pathTracer.beginDraw();
+    this.pathTracer.translate(this.position.x, this.position.y);
+
+    this.pathTracer.stroke(100, 255, 255, 150);
+    this.pathTracer.strokeWeight(3);
+    this.renderPath();
+
+    this.pathTracer.endDraw();
+  }
+
+  private void renderPath()
+  {
+    if (this.path != null)
+    {
+      for (var pathPoint : this.path)
+        this.pathTracer.point(pathPoint.x, pathPoint.y);
+    }
+  }
+}
